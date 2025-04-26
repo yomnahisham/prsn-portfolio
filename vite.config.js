d13997007ccfd3223173f1792182ab919e11c22a
yomna <yomnaothman@aucegypt.edu>
2025-04-26 20:18:05 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/prsn-portfolio/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
